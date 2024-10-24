6a0c499940d1f5e027b5dfb7c77041e4d79c6864
egoist945402376 <egoist945402376@gmail.com>
2024-10-24 18:05:44 +0100

    Chapter 9

---
+++ C9.py
@@ -0,0 +1,19 @@
+import sys
+
+def greet():
+    # argv: a list of command line arguments passed to a Python script
+    # argv[0] is the script name
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello to the one running this function!')
+    elif len(args) == 2:
+        print(f"Hello, nice to meet you, {args[1]}!")
+    else:
+        print('There are too many arguments!')
+
+def main():
+    greet()
+
+
+if __name__ == '__main__':
+    main()
